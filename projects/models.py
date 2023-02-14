485ce95120f2a7f85e960606462decd161cb1f78
Chao-Yen Huang <chaoyen510220@gmail.com>
2023-02-14 23:25:42 +0800

    finish latest post section except the category tag color

--- projects/models.py
+++ projects/models.py
@@ -3,10 +3,10 @@ from core.models import PostBase
 
 # Create your models here.
 class ProjectPost(PostBase):
-    Field=(
+    differentField=(
         ("Machine Learning","Machine Learning"),
         ("BlockChain ","BlockChain"),
         ("3D Art","3D Art"),
     )
-    field = models.CharField(max_length=20,choices=Field)
+    topic = models.CharField(max_length=20,choices=differentField)
     just_test = models.CharField(null=True,blank=True,max_length=100)
