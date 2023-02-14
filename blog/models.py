6acf977e3ae140e7c51b97ba5f6e61abcdef3912
Chao-Yen Huang <chaoyen510220@gmail.com>
2023-02-14 19:57:44 +0800

    add app models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from core.models import PostBase
 
 # Create your models here.
+class BlogPost(PostBase):
+    pass
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class PostBase(models.Model):
+    CATETORIES=(
+        ('B','Blog'),
+        ('P','Photo'),
+        ('T','Travel'),
+        ('R','Review'),
+        ('Pr','Project'),
+    )
+    title = models.CharField(max_length=50)
+    category = models.CharField(max_length=2,choices=CATETORIES)
+    description = models.CharField(max_length=100)
+    post_time = models.DateTimeField(auto_now_add=True)
