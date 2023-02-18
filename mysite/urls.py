12d305d43d78c4009a413df3bc6c9b382986efac
Chao-Yen Huang <chaoyen510220@gmail.com>
2023-02-18 20:30:08 +0800

    adjust navbar & photo template

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('',include('core.urls')),
     path('admin/', admin.site.urls),
     path('projects/',include('projects.urls')),
+    path('photos/',include('photo.urls')),
     path('travel/',include('travel.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
