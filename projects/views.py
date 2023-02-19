c942f9c3f816f7915580177d389d85ba25dbb858
Chao-Yen Huang <chaoyen510220@gmail.com>
2023-02-19 20:38:39 +0800

    add new feature and some dummy data to project detail view

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from .models import ProjectPost
+from .models import ProjectPost,ProjectPostSection
 
 
 class ProjectPageView(TemplateView):
@@ -18,5 +18,7 @@ class ProjectDetailView(TemplateView):
     def get_context_data(self, *args,**kwargs):
         
         context=super().get_context_data(*args,**kwargs)
-        context['projectMain']=ProjectPost.objects.get(pk=kwargs.get('pk'))
+        project = ProjectPost.objects.get(pk=kwargs.get('pk'))
+        context['projectMain']= project
+        context['sections']=ProjectPostSection.objects.filter(belongPost=kwargs.get('pk'))
         return context
