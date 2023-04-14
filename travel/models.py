6d3be99e0bdeabb8724ce8d332b0ec8048eb3f0d
Chao-Yen Huang <chaoyen510220@gmail.com>
2023-04-15 02:02:12 +0800

    feat: add travel deatil page

--- travel/models.py
+++ travel/models.py
@@ -5,3 +5,10 @@ from core.models import PostBase
 class TravelPost(PostBase):
     location=models.CharField(max_length=100)
     country=models.CharField(max_length=100)
+
+class TravelPostSection(models.Model):
+    belongPost = models.ForeignKey(TravelPost,on_delete=models.CASCADE)
+    section_num = models.IntegerField(null=True)
+    section_title = models.CharField(null=True,blank=True,max_length=100)
+    section_context = models.TextField(null=True,blank=True)
+    section_image = models.ImageField(null=True,blank=True,upload_to="images/travels/")
