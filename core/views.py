6181f289ae369a30caa420013d2cd6d74e26701b
Chao-Yen Huang <chaoyen510220@gmail.com>
2023-04-16 00:20:36 +0800

    feat: ajax for home page latest project navbar

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.http import JsonResponse
+from django.core import serializers
 from .models import PostBase
 from travel.models import TravelPost
 from blog.models import BlogPost
@@ -9,30 +11,70 @@ from photo.models import PhotoPost
 
 class HomePageView(TemplateView):
 
-    template_name="home.html"
+        template_name="home.html"
+        latest_post = PostBase.objects.none()
+        tmp = PostBase.objects.none()
 
-    def get_context_data(self, *args,**kwargs):
+        def get(self, *args,**kwargs):
 
-                context=super().get_context_data(*args,**kwargs)
-                latest_post=PostBase.objects.values_list('id','category').order_by('-id')[:4]
+                context=super().get_context_data(**kwargs)
+                selection=self.request.GET.get('selection')
                 
-                latest_post=list(latest_post)
-                latest_post.reverse()
-
-                final = PostBase.objects.none()
-                for i in range(len(latest_post)):
-                        latest_post_id=latest_post[i][0]
-                        latest_post_category=latest_post[i][1]
-                        if latest_post_category == "Blog":
-                                tmp=BlogPost.objects.filter(id=latest_post_id)
-                        elif latest_post_category == "Travel":
-                                tmp=TravelPost.objects.filter(id=latest_post_id)
-                        elif latest_post_category == "Project":
-                                tmp=ProjectPost.objects.filter(id=latest_post_id)
-                        elif latest_post_category == "Photo":
-                                tmp=PhotoPost.objects.filter(id=latest_post_id)
+                if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                        if selection == "All":
+                                latest_post=PostBase.objects.values_list('id','category').order_by('-id')[:4]
+                                latest_post=list(latest_post)
+                                final = PostBase.objects.none()
+                                for i in range(len(latest_post)):
+                                        latest_post_id=latest_post[i][0]
+                                        latest_post_category=latest_post[i][1]
+                                        if latest_post_category == "Blog":
+                                                tmp=BlogPost.objects.filter(id=latest_post_id)
+                                        elif latest_post_category == "Travel":
+                                                tmp=TravelPost.objects.filter(id=latest_post_id)
+                                        elif latest_post_category == "Project":
+                                                tmp=ProjectPost.objects.filter(id=latest_post_id)
+                                        elif latest_post_category == "Photo":
+                                                tmp=PhotoPost.objects.filter(id=latest_post_id)
+                                        final=final.union(tmp)
+                                context['posts']=final
+                                return self.render_to_response(context)
+                                
+                                
+                        elif selection == "Travel":
+                                latest_post=PostBase.objects.values_list('id','category').order_by('-id').filter(category="Travel")[:4]
+                                print(type(latest_post))
+                                final = PostBase.objects.none()
+                                for i in range(len(latest_post)):
+                                        latest_post_id=latest_post[i][0]
+                                        latest_post_category=latest_post[i][1]
+                                        print(latest_post_id)
+                                        tmp=TravelPost.objects.filter(id=latest_post_id)
+                                        final=final.union(tmp)
+                                #context['posts']=final 
+                                finaljson = serializers.serialize('json',final)
+                                return JsonResponse(finaljson,safe=False)
                         
+                
+                else:
+                        latest_post=PostBase.objects.values_list('id','category').order_by('-id')[:4]
+                        latest_post=list(latest_post)
+                        #latest_post.reverse()
+                        print(latest_post)
+                        final = PostBase.objects.none()
+                        for i in range(len(latest_post)):
+                                latest_post_id=latest_post[i][0]
+                                latest_post_category=latest_post[i][1]
+                                if latest_post_category == "Blog":
+                                        tmp=BlogPost.objects.filter(id=latest_post_id)
+                                elif latest_post_category == "Travel":
+                                        tmp=TravelPost.objects.filter(id=latest_post_id)
+                                elif latest_post_category == "Project":
+                                        tmp=ProjectPost.objects.filter(id=latest_post_id)
+                                elif latest_post_category == "Photo":
+                                        tmp=PhotoPost.objects.filter(id=latest_post_id)
+                                
 
-                        final=final.union(tmp)
-                context['posts']=final
-                return context
+                                final=final.union(tmp)
+                        context['posts']=final
+                        return self.render_to_response(context)
--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,43 @@
+$(document).ready(function(){
+    $.ajax({          
+        type:"GET",
+        url:$(".sidebar").attr("data-url"),
+        data : {
+            "selection": "All"
+        },
+        dataType: 'json',
+        success:function(response){
+            var rep = JSON.parse(response);
+            $.each(rep, function(m, item){
+                console.log(m,item);
+            });
+        },
+        failure: function() {console.log("Error");}
+    });
+});
+    
+
 $(function() {
     $(".latest-nav").click(function() {
-       $(".latest-nav").removeClass("active");
-       $(this).addClass("active");
+        $(".latest-nav").removeClass("active");
+        $(this).addClass("active");
+        $.ajax({
+            
+            type:"GET",
+            url:$(".sidebar").attr("data-url"),
+            data : {
+                "selection": $(this).text()
+            },
+            dataType: 'json',
+            success:function(response){
+                
+                let rep = JSON.parse(response);
+                console.log(rep);
+                $.each(rep, function(m, item){
+                   
+                });
+            },
+            failure: function() {console.log("Error");}
+        });
     });
 });
