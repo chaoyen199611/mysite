5c79da1a3f17b05e45a901977b1f5552429a2c29
Joey Huang <chaoyen510220@gmail.com>
2023-05-21 23:40:26 +0800

    feat: add dynamic field to project post creation

--- static/js/project.js
+++ static/js/project.js
@@ -1,15 +1,36 @@
+let section = 0;
 $(document).ready(function() {
     let form = $('#create-post');
     let addButton = $('#add-field');
     
     addButton.click(function() {
-        let inputName = 'field' + (form.children().length);
+        section++;
+        console.log(section);
+        let inputName = 'Section' + section;
         console.log(inputName);
         let divblock = '<div class="form-field">'+inputName+'</div>';
         form.append(divblock)
         let newField = $('<input>')
             .attr('name', inputName)
+            .attr('type', 'number');
+        form.append(newField);
+        let sectiontitle = '<div class="form-field">Section Title</div>';
+        form.append(sectiontitle)
+        newField = $('<input>')
+            .attr('name', "sectiontitle").attr('maxlength',100)
             .attr('type', 'text');
+        form.append(newField);
+        let sectioncontext = '<div class="form-field">Section Context</div>';
+        form.append(sectioncontext)
+        newField = $('<input>')
+            .attr('name', "sectiontitle")
+            .attr('type', 'text');
+        form.append(newField);
+        let sectionimage = '<div class="form-field">Section Image</div>';
+        form.append(sectionimage)
+        newField = $('<input>')
+            .attr('name', "sectionimage").attr('onchange','PreviewImage()').attr('accept','image/*')
+            .attr('type', 'file');
         form.append(newField);
     });
 });
