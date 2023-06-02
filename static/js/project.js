7d5bf00231bfb3e26a15a9c094455110fc9e5782
Joey Huang <chaoyen510220@gmail.com>
2023-06-02 17:26:26 +0800

    style: create tag-like category css

--- static/js/project.js
+++ static/js/project.js
@@ -10,9 +10,21 @@ $(document).ready(function() {
         this.style.height = 'auto';
         this.style.height = (this.scrollHeight) + 'px';
     });
-    addButton.click(function() {
-        section++;
-        console.log(section);
+
+    const category = document.querySelectorAll(".project-field").forEach(function (el){
+        console.log(el.textContent);
+        if(el.textContent == "Machine Learning"){
+            el.style.background = "aqua";
+        }
+        else if(el.textContent == "BlockChain"){
+            el.style.color = "#de7010";
+        }
+        else if(el.textContent == "3D Art"){
+            el.style.color = "#6e14b8";
+        }
+        else{
+            el.style.color = "#b814b2";
+        }
     });
 });
 
@@ -24,3 +36,9 @@ function PreviewImage() {
         document.getElementById("preview-image").src = oFREvent.target.result;
     };
 };
+
+let colorMatch = {
+    'Machine Learning':'blue',
+    '20-59'    : 'orange',
+    '60-100'   : 'green'
+ };
