48632b12ce0de9c571d9fd0f69dc32c41796f48e
Joey Huang <chaoyen510220@gmail.com>
2023-06-04 14:29:00 +0800

    style: add project detail article nav

--- static/js/base.js
+++ static/js/base.js
@@ -2,3 +2,21 @@ $(document).ready(function(){
     $(".navbarlist").removeClass("active");
     
 });
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("article-nav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
